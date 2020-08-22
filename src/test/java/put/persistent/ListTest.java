d46b27f7784fa30cefc5cac402ca898ea5d43cfa
Piotr Siwinski <piotr.a.siwinski@allegro.pl>
2020-08-22 16:54:18 +0200

    Write tests for getting elements from list

--- src/test/java/put/persistent/ListTest.java
+++ src/test/java/put/persistent/ListTest.java
@@ -1,15 +1,14 @@
 package put.persistent;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +16,7 @@ public class ListTest {
 
 
     final static String pathToHeap = "listHeap.pool";
+    final static String listName = "my-numbers";
 
     @AfterAll
     public static void cleanup() {
@@ -24,44 +24,92 @@ public class ListTest {
         if (f.exists()) {
             // f.delete();
         }
-
-        var lst = new ArrayList<Integer>();
-
-        lst.add(1);
-        lst.get(0);
     }
-
 
     @Test
     @Order(1)
-    public void shouldAddToEmptyList(){
+    public void shouldAddToEmptyList() {
         var heap = new FileHeap(Paths.get(pathToHeap));
-        var lst = new PersistentList(heap);
-
-
+        var lst = new PersistentList(listName, heap);
         lst.add(1);
+
+        int e1 = lst.get(0);
+
+        assertEquals(1, e1);
+    }
+
+    @Test
+    @Order(2)
+    public void shouldGetElementFromList() {
+        var heap = new FileHeap(Paths.get(pathToHeap));
+        var lst = new PersistentList(listName, heap);
+
+        int e1 = lst.get(0);
+
+        assertEquals(1, e1);
+    }
+
+    @Test
+    @Order(3)
+    public void shouldAddAnotherElements() {
+        var heap = new FileHeap(Paths.get(pathToHeap));
+        var lst = new PersistentList(listName, heap);
+
         lst.add(2);
         lst.add(3);
         lst.add(4);
 
+        int e1 = lst.get(0);
+        int e2 = lst.get(1);
+        int e3 = lst.get(2);
+        int e4 = lst.get(3);
 
-        int first = lst.get(0);
+        assertEquals(1, e1);
+        assertEquals(2, e2);
+        assertEquals(3, e3);
+        assertEquals(4, e4);
+    }
 
+    @Test
+    @Order(3)
+    public void shouldGetAllElements() {
+        var heap = new FileHeap(Paths.get(pathToHeap));
+        var lst = new PersistentList(listName, heap);
 
-        assertEquals(1, first);
+        int e1 = lst.get(0);
+        // wywala sie, bo musi doczytac z pmem kolejny element, a nie ma w pamieci
+        int e2 = lst.get(1);
+        int e3 = lst.get(2);
+        int e4 = lst.get(3);
 
+        assertEquals(1, e1);
+        assertEquals(2, e2);
+        assertEquals(3, e3);
+        assertEquals(4, e4);
     }
 
 
     private static class PersistentList {
         private static int nodeId = 1;
-        private static final String headName = "head";
-
         private final Heap heap;
+        private final ListMetadata listMetadata;
         private Node head;
 
-        public PersistentList(Heap heap) {
+        public PersistentList(String name, Heap heap) {
             this.heap = heap;
+
+            ListMetadata metadata = heap.getObject(name, ListMetadata.class);
+            if (metadata == null) {
+                // first creation of list
+                listMetadata = new ListMetadata(name, null);
+                heap.putObject(name, listMetadata);
+            } else {
+                // list was already created
+                listMetadata = metadata;
+                if(metadata.head != null) { // but can be empty, so null is possible
+                    head = listMetadata.head;
+                }
+            }
         }
 
         public void add(int element) {
@@ -69,9 +117,11 @@ public class ListTest {
                 if (head == null) {
                     head = new Node("node-" + nodeId++, element);
                     heap.putObject(head.id, head);
+                    listMetadata.head = head;
+                    heap.putObject(listMetadata.name, listMetadata);
                 } else {
                     Node help = head;
-                    while (help.next != null){
+                    while (help.next != null) {
                         help = help.next;
                     }
                     Node newNode = new Node("node-" + nodeId++, element);
@@ -84,17 +134,23 @@ public class ListTest {
         }
 
 
-        public int get(int index){
-            Node tmp = head;
-            for(int i =0; i < index; i++){
+        public int get(int index) {
+            if (head == null) {
+                throw new ArrayIndexOutOfBoundsException("List is empty");
+            }
+            Node tmp = head; // trzeba doczytac kolejne elementy z pamieci
+            for (int i = 0; i < index; i++) {
                 tmp = tmp.next;
+                if(tmp == null){
+                    throw new RuntimeException("Cannot get element...");
+                }
             }
             return tmp.element;
         }
 
-        @Getter
-        @Setter
+        @Data
         @NoArgsConstructor
+        @AllArgsConstructor
         static class Node {
             private String id;
             private int element;
@@ -104,6 +160,14 @@ public class ListTest {
                 this.element = element;
                 this.id = id;
             }
+        }
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        static class ListMetadata {
+            private String name;
+            private Node head;
         }
     }
 }
